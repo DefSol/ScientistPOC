7576be05b627254c2ffad60d7227810361f20fdc
Reuben Dunn <rdunn@spendvision.com>
2016-05-25 16:50:46 +1200

    Added serilog anf started resultpub

---
+++ POC/ResultPublisher.cs
@@ -0,0 +1,17 @@
+﻿using GitHub;
+using System.Threading.Tasks;
+using Serilog;
+
+namespace POC
+{
+    public class ResultPublisher : IResultPublisher
+    {
+        public Task Publish<T>(Result<T> result)
+        {
+            var logger = new LoggerConfiguration()
+                    .WriteTo.ColoredConsole()
+                    .CreateLogger();
+
+        }
+    }
+}
